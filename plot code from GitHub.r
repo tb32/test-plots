d0099b2716247b51c61ad433001c937c8d266db9
tb32 <tb32@users.noreply.github.com>
2017-07-17 11:32:42 -0400

    Add some additional points

--- plot code from GitHub.r
+++ plot code from GitHub.r
@@ -5,3 +5,7 @@ points(17,8, col = 2, pch = 15)
 
 ### Make plot a line graph
 plot(foo.x, foo.y, type = 'l', lwd = 2, col = 3)
+
+
+### Add some additional points
+points(c(16,18), c(8,3), col = 2, pch = 17, cex = 1.3)
